66a57831459ff3153ac561e4065255226348b671
soulen77 <soulen77@gmail.com>
2025-04-11 00:30:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,4 +21,4 @@ fi
 
 # Start the Django server with production-ready configurations
 echo "Starting Django server with Gunicorn..."
-exec /updateCronCircus.sh --bind 0.0.0.0:8080
+exec python /app/manage.py collectstatic --noinput --bind 0.0.0.0:8080
