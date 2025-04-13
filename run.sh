1ce61f9f54a0e3be81d90d5efa7ee84258a5f022
soulen77 <soulen77@gmail.com>
2025-04-13 16:26:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ export PYTHONPATH="$FERMENTRACK_DIR"
 # If settings_local.py doesn't exist, generate it from example
 if [ ! -f "$FERMENTRACK_DIR/settings_local.py" ]; then
     echo "Generating default settings_local.py..."
-    cp settings_local.py "$FERMENTRACK_DIR/settings_local.py"
+    cp fermentrack/settings_local.py "$FERMENTRACK_DIR/settings_local.py"
 fi
 
 # Run database migrations
