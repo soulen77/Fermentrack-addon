d95ed68df5d35b33cdbc8485f3798d7772124207
soulen77 <soulen77@gmail.com>
2025-04-12 19:35:12 -0500

    Update settings_local.py

--- Fermentrack/settings_local.py
+++ Fermentrack/settings_local.py
@@ -1,8 +1,17 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FERMENTRACK_BASE = "/data/fermentrack"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/data', 'db.sqlite3'),
+        'NAME': os.path.join(FERMENTRACK_BASE, 'db.sqlite3'),
     }
 }
+
+STATIC_ROOT = os.path.join(FERMENTRACK_BASE, 'static')
+MEDIA_ROOT = os.path.join(FERMENTRACK_BASE, 'media')
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
