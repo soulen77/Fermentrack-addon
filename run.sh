023730cd53b907f659eb764d4e3b5b8303253fa8
soulen77 <soulen77@gmail.com>
2025-04-18 18:26:37 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,7 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-cd /app
+# Ensure that the database is initialized (only run once)
+python3 /app/manage.py migrate --noinput || true
 
-# Set environment variable for Django to find the settings
-export DJANGO_SETTINGS_MODULE=fermentrack_django.settings
-
-# Run Django migrations and collect static files
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
-
-# Start Gunicorn
-gunicorn fermentrack_django.wsgi:application \
-    --bind 0.0.0.0:8080 \
-    --workers 3
+# Start the Gunicorn server
+exec gunicorn fermentrack_django.wsgi:application --bind 0.0.0.0:8080
