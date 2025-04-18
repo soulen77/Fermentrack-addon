a1db7ae2c59105a96bda321e0e6a553546281ff6
soulen77 <soulen77@gmail.com>
2025-04-18 15:38:30 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,4 +21,4 @@ done
 
 # Start the Django server with production-ready configurations
 echo "Starting web server"
-exec fermentrack.wsgi:application --bind 0.0.0.0:8080 --workers 3 --log-level info
+exec gunicorn fermentrack.wsgi:application --bind 0.0.0.0:8080 --workers 3 --log-level info
