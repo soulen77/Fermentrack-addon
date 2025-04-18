f92168219bffb356a8f8435bc9a5401aadfa0954
soulen77 <soulen77@gmail.com>
2025-04-18 17:24:50 -0500

    Delete scripts directory

--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# /scripts/run.sh
-
-# Ensure that virtual environment is activated
-source /app/fermentrack/venv/bin/activate
-
-# Run the Fermentrack application (this is optional, if you want to run additional tasks before Fermentrack)
-exec gunicorn --bind 0.0.0.0:8080 fermentrack.wsgi:application
