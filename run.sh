bbd37117e27fe2dc258c17322de6ac1ecdbdc2f5
soulen77 <soulen77@gmail.com>
2025-04-18 21:10:02 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 cd /app
 
+# Set a default secret key if none is provided
+export DJANGO_SECRET_KEY="${DJANGO_SECRET_KEY:-changeme123}"
+
 # Activate the virtual environment
 . /app/venv/bin/activate
 
-# Apply database migrations
+# Run migrations
 python3 manage.py migrate --noinput
 
-# Start Gunicorn as PID 1
-exec gunicorn fermentrack_django.wsgi:application --bind 0.0.0.0:8080
+# Start Gunicorn using the venv version
+exec /app/venv/bin/gunicorn fermentrack_django.wsgi:application --bind 0.0.0.0:8080
