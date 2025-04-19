d375646a622a652245ff0da9487e93322f5bc6d7
soulen77 <soulen77@gmail.com>
2025-04-18 20:35:26 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd /app
 
-# Ensure database migrations are applied
+# Apply database migrations
 python3 manage.py migrate --noinput
 
-# Start Gunicorn with the correct WSGI app
-gunicorn fermentrack_django.wsgi:application --bind 0.0.0.0:8080
+# Start Gunicorn properly (as PID 1)
+exec gunicorn fermentrack_django.wsgi:application --bind 0.0.0.0:8080
