1dcf3c81b412e4c130545b0be7fdecc449003d27
soulen77 <soulen77@gmail.com>
2025-04-18 20:38:08 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 cd /app
 
+# Activate the virtual environment
+. /app/venv/bin/activate
+
 # Apply database migrations
 python3 manage.py migrate --noinput
 
-# Start Gunicorn properly (as PID 1)
+# Start Gunicorn as PID 1
 exec gunicorn fermentrack_django.wsgi:application --bind 0.0.0.0:8080
